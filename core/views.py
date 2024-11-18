165cf429f315aa95b2f5bc4d55aa918f5b8e55e5
Andre Luis da Cunha <andreluiscunha81@gmail.com>
2024-11-18 01:43:04 -0300

    Implement user signup functionality
    - Updated the signup view to handle form submissions via POST.
    - Added validation checks for:
      - Duplicate email and username
      - Password and password confirmation match
    - Save new users to the database upon successful validation.
    - Ensured the signup.html form is fully functional.

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -8,4 +10,30 @@ def login(request):
     return render(request, 'login.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exists')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                # return redirect('login')
+
+                #login user
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                return redirect('/')
+        else:
+            messages.info(request, 'Password not matching')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
